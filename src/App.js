8e03ccb04a7d05a632cba6fbe807fa69719b6bbc
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-01-28 13:16:55 +0700

    Displaying Items From Data With Render List

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const listItem = [
+    { id: 1, title: "eat", done: true },
+    { id: 2, title: "sleep", done: true },
+    { id: 3, title: "code", done: true },
+    { id: 4, title: "repeat", done: true },
+];
+
 function App() {
     return (
         <div className="app">
@@ -9,11 +16,49 @@ function App() {
     );
 }
 
-function Logo() {}
+function Logo() {
+    return <span className="logo">📖 Gocheck ✅</span>;
+}
 
-function Form() {}
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>Ada yang mau dicatat ga? 🤔</h3>
+        </div>
+    );
+}
 
-function Checklist() {}
+function Checklist() {
+    return (
+        <div className="list">
+            <ul>
+                {listItem.map((item) => (
+                    <ShowItemList key={item.id} item={item} />
+                ))}
+            </ul>
+        </div>
+    );
+}
 
-function Stats() {}
+function ShowItemList({ item }) {
+    return (
+        <li>
+            <input type="checkbox" />
+            <span style={{ textDecoration: item.done ? "line-through" : "" }}>
+                {item.title}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <span>
+                Kamu punya x catatan dan baru x yang dichecklist (%x) ✅
+            </span>
+        </footer>
+    );
+}
 export default App;
