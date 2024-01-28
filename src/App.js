ed872fabd521d544f1abb7435906d44b5af61f6c
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-01-28 18:59:03 +0700

    separate components into their respective files

--- src/App.js
+++ src/App.js
@@ -1,85 +1,56 @@
 import { useState } from "react";
-
-const listItem = [
-    { id: 1, title: "eat", done: true },
-    { id: 2, title: "sleep", done: true },
-    { id: 3, title: "code", done: true },
-    { id: 4, title: "take a shower", done: false },
-    { id: 5, title: "repeat", done: true },
-];
-
-const totalTrueValues = listItem.filter((item) => item.done).length;
-const totalItems = listItem.length;
-const percentageTrueValues = (totalTrueValues / totalItems) * 100;
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import CheckList from "./components/CheckList";
+import Stats from "./components/Stats";
 
 function App() {
+    const [listItems, setListItems] = useState([]);
+
+    function handleAddItem(item) {
+        setListItems((listItems) => [...listItems, item]);
+    }
+
+    function handleDeleteItem(id) {
+        setListItems((listItems) => listItems.filter((item) => item.id !== id));
+    }
+
+    function handleToggleItem(id) {
+        setListItems((listItems) => {
+            return listItems.map((item) => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        done: !item.done,
+                    };
+                }
+                return item;
+            });
+        });
+    }
+
+    function handleClearItems() {
+        const confirm = window.confirm(
+            "Are you sure you want to clear the list?"
+        );
+        if (confirm) {
+            setListItems([]);
+        }
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <Checklist />
-            <Stats />
-        </div>
-    );
-}
-
-function Logo() {
-    return <span className="logo">📖 Gocheck ✅</span>;
-}
-
-function Form() {
-    const [title, setTitle] = useState("");
-    function handleSubmit(e) {
-        e.preventDefault();
-        // console.log(e);
-    }
-    return (
-        <form className="add-form" onSubmit={handleSubmit}>
-            <h3>Ada yang mau dicatat ga? 🤔</h3>
-            <input
-                type="text"
-                name="title"
-                id=""
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+            <Form onAddItem={handleAddItem} />
+            <CheckList
+                items={listItems}
+                onDeleteItem={handleDeleteItem}
+                onToggleItem={handleToggleItem}
+                onClearItems={handleClearItems}
             />
-            <button>Add</button>
-        </form>
-    );
-}
-
-function Checklist() {
-    return (
-        <div className="list">
-            <ul>
-                {listItem.map((item) => (
-                    <ShowItemList key={item.id} item={item} />
-                ))}
-            </ul>
+            <Stats items={listItems} />
         </div>
     );
 }
 
-function ShowItemList({ item }) {
-    return (
-        <li>
-            <input type="checkbox" />
-            <span style={{ textDecoration: item.done ? "line-through" : "" }}>
-                {item.title}
-            </span>
-            <button>❌</button>
-        </li>
-    );
-}
-
-function Stats() {
-    return (
-        <footer className="stats">
-            <span>
-                Kamu punya {listItem.length} catatan dan baru {totalTrueValues}{" "}
-                yang dichecklist ({percentageTrueValues}%) ✅
-            </span>
-        </footer>
-    );
-}
 export default App;
